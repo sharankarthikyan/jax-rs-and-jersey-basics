d89a77a56370630afcc734f4a0ff752169980167
sharankarthikeyan <karthikeyansharan00@gmail.com>
2023-11-19 10:14:55 +0530

    added context param

--- src/main/java/org/sharan/showroom/resources/Demo.java
+++ src/main/java/org/sharan/showroom/resources/Demo.java
@@ -1,14 +1,24 @@
 package org.sharan.showroom.resources;
 
+
 import jakarta.ws.rs.*;
-import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.*;
 
 @Path("/")
+@Produces(MediaType.TEXT_PLAIN)
 public class Demo {
 	
+	@Context
+	private UriInfo uriInfo;
+	
 	@GET
-	@Produces(MediaType.TEXT_PLAIN)
 	public String dummy(@HeaderParam("HeaderValue") String headerValue, @CookieParam("_csrf") String csrf) {
 		return headerValue + " " + csrf;
+	}
+	
+	@GET
+	@Path("demo")
+	public String contextDemo(@Context HttpHeaders headers) {
+		return uriInfo.getAbsolutePath().toString() + " - " + headers.getRequestHeaders().toString();
 	}
 }
